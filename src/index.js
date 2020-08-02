1bca8aeeb9d28331add74d5a8668bf4a12363114
Adeel <adeelbhatti88@gmail.com>
2020-08-02 15:56:26 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1>hello world</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
+//left off at 20:42
